b6ad7a3af048e7d59858e0d75825be00c6ca525d
mikeHelderal <mike.helderal.sio@gmail.com>
2024-12-11 14:39:33 +0100

    ajustement cors

--- back_projet/App.js
+++ back_projet/App.js
@@ -35,7 +35,7 @@ app.use(bodyParser.text({type: '/'}));
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", env.url_front);
+  res.setHeader("Access-Control-Allow-Origin", env.cors_origin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
--- back_projet/config/config.js
+++ back_projet/config/config.js
@@ -12,4 +12,5 @@ export const env = {
   db_dialect: process.env.DB_DIALECT,
   db_password: process.env.MYSQL_ROOT_PASSWORD,
   url_front: process.env.URL_FRONT,
+  cors_origin: process.env.CORS_ORIGIN
 }
